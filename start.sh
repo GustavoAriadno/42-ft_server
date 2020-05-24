8a53b96635b42e9439285afe6033192362cdd99d
GustavoAriadno <gugu.gustavo2011@gmail.com>
2020-05-24 04:50:37 -0300

    final version

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t ft_server-image .
-docker run --name ft_server-container -it -p 80:80 -p 443:443 ft_server-image
+docker run --name ft_server-container -itd -p 80:80 -p 443:443 ft_server-image
 docker ps
